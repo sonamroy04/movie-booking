0325e778564acb3e48f2b5c85e436f67285181a9
Sonam <78811627+sonamroy04@users.noreply.github.com>
2022-10-17 13:23:30 +0530

    Add files via upload

---
+++ Movies1Application.java
@@ -0,0 +1,13 @@
+package com.example.MyMoviePlan.Movies1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movies1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movies1Application.class, args);
+	}
+
+}
---
+++ Movies1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyMoviePlan.Movies1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Movies1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
